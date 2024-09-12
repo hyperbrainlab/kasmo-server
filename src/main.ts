98b95bcdfb049c8752ecfa18023ce0d8a1063a29
Dong-ri, Shin <tlsehdfl@naver.com>
2024-09-12 16:30:02 +0900

    🔒️ enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import { TransformInterceptor } from 'src/modules/common/interceptor/transform';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 import { AppModule } from './app.module';
 const PORT = 8080;
@@ -16,10 +17,18 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
 
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  };
+
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors(corsOptions);
   await app.listen(PORT);
 }
 bootstrap();
